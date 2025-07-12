4339cb35e68ad0c2cc1a75a12c93f8ae0b5f19eb
rajathnh <rajathnh2@gmail.com>
2025-07-12 09:51:38 +0530

    Initial setup

---
+++ backend/app.js
@@ -0,0 +1,21 @@
+// backend/server.js
+
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Connect to the database
+connectDB();
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
